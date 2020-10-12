0a679f1f7e92cc3f09758567b793733ec3cd821d
cmatchneer <cmatchneer@yahoo.com>
2020-10-12 11:55:43 -0400

    head drawn small issues worked out

--- app/assets/javacript/easy.js
+++ app/assets/javacript/easy.js
@@ -2,28 +2,63 @@ var $myCanvas = $('#theMan');
 var $theWords = $('#theWords');
 
 function easy(num) {
-    console.log(num);
-    $myCanvas.drawArc({
-        strokeStyle: "black",
-        strokeWidth: 1,
-        x: 270,
-        y: 38,
-        radius: 10,
-    }).drawArc({
+
+    if (num === 14) {
+        //head
+        console.log(num);
+        $myCanvas.drawArc({
+            strokeStyle: "black",
+            strokeWidth: 1,
+            x: 270,
+            y: 38,
+            radius: 10,
+        })
+    }
+    if (num === 12) {
         // draw the left eye
-        fillStyle: '#333',
-        strokeStyle: '#333',
-        x: 265,
-        y: 37,
-        radius: 1.5
-    }).drawArc({
+        $myCanvas.drawArc({
+            fillStyle: '#333',
+            strokeStyle: '#333',
+            x: 265,
+            y: 37,
+            radius: 1.5
+        })
+    }
+    if (num === 10) {
+        $('#theWords').removeLayer('text').drawLayers();
         // draw the right eye
-        fillStyle: '#333',
-        strokeStyle: '#333',
-        x: 275,
-        y: 37,
-        radius: 1.5
-    })
+        $myCanvas.drawArc({
+            fillStyle: '#333',
+            strokeStyle: '#333',
+            x: 275,
+            y: 37,
+            radius: 1.5
+        })
+        $theWords.drawRect({
+            fillStyle: 'teal',
+            strokeStyle: 'gold',
+            strokeWidth: 1,
+            x: 0,
+            y: 0,
+            fromCenter: false,
+            width: 200,
+            height: 50
+        });
+        $theWords.drawText({
+            layer: true,
+            name: "text",
+            text: "Well there is still lots of time",
+            fontfamily: "cursive",
+            fontSize: 15,
+            x: 100,
+            y: 25,
+            strokeStyle: "black",
+            strokeWidth: 1,
+            fillStyle: "black"
+        })
+    }
+
+
 }
 
 export { easy }
