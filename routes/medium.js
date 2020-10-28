a5dbf89ca122bc60c69c869d513c4633eacb37e9
cmatchneer <cmatchneer@yahoo.com>
2020-10-28 15:10:08 -0400

    new words and tips

--- routes/medium.js
+++ routes/medium.js
@@ -2,15 +2,15 @@ module.exports = function(app) {
 
     app.get("/api/medium", function(req, res) {
         res.json(
-            [{ word: "the", tip: "You use this word everyday starts with t" },
-                { word: "jumbo", tip: "Some shrimps are called this" },
-                { word: "javascript", tip: "Used to make a web page dyanmic" },
-                { word: "html", tip: "Basics of makeing a website" },
-                { word: "css", tip: "Makes webpages look goooooooood" },
-                { word: "gravity", tip: "What keeps us from being able to fly" },
-                { word: "codeing", tip: "Another name for programming" },
-                { word: "bugs", tip: "The one thing coders and all homeowners hate to see" },
-                { word: "debugging", tip: "What coders spend 90% of their lives doing" }
+            [{ word: "annoying ", tip: "When someone wont stop bothering you" },
+                { word: "tomato", tip: "Red fruit commonaly mistaken as veggie" },
+                { word: "harmony", tip: "Everything is working in perfect _____" },
+                { word: "superman", tip: "The man of steel" },
+                { word: "television", tip: "TV is short for" },
+                { word: "baseball", tip: "Americas past time" },
+                { word: "backbone", tip: "Grow a ____ and say something" },
+                { word: "capitalism", tip: "The idea you own your own labor" },
+                { word: "dapper", tip: "You look ____ today" }
             ])
     })
 };
